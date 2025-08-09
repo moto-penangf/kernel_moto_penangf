6b6374fe44ef7fdcfbf445829153371529e596f1
progzone122 <progzone122@gmail.com>
2025-08-09 22:41:32 +0000

    penangf: improve helper srcipts

--- helpers/download_gki.sh
+++ helpers/download_gki.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
+set -e
+
 my_top_dir=$PWD
 
-mkdir -vp $my_top_dir/kernel
-cd $my_top_dir/kernel
+echo "[I] Creating kernel build structure..."
+mkdir -vp "$my_top_dir/kernel"
+cd "$my_top_dir/kernel"
 git clone https://android.googlesource.com/kernel/build
 
-mkdir -vp  $my_top_dir/kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b
-cd $my_top_dir/kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b
+echo "[I] Setting up Clang toolchain..."
+mkdir -vp "$my_top_dir/kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b"
+cd "$my_top_dir/kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b"
 
-if [ ! -d "${my_top_dir}/prebuilts/clang" ]; then
-    echo "[I] Clang is not downloaded, downloading..."
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/b669748458572622ed716407611633c5415da25c/clang-r416183b.tar.gz
-    tar -xf $my_top_dir/prebuilts-master/clang/host/linux-x86/clang-r416183b/clang-r416183b.tar.gz
+if [ ! -d "$my_top_dir/kernel/prebuilts-master/clang/host/linux-x86/clang-r416183b/bin" ]; then
+    echo "[I] Clang not found, downloading..."
+    wget -O clang-r416183b.tar.gz \
+      https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/b669748458572622ed716407611633c5415da25c/clang-r416183b.tar.gz
+    tar -xf clang-r416183b.tar.gz --strip-components=1
 else
-    echo "[✔] Clang already exist, copying..."
-    cp -r $my_top_dir/prebuilts/clang/host/linux-x86/clang-r416183b/* .
+    echo "[✔] Clang already exists."
 fi
 
-mkdir -vp  $my_top_dir/kernel/prebuilts/
-cd $my_top_dir/kernel/prebuilts/
-git clone https://android.googlesource.com/kernel/prebuilts/build-tools
-cd $my_top_dir
+echo "[I] Downloading build-tools..."
+mkdir -vp "$my_top_dir/kernel/prebuilts/"
+cd "$my_top_dir/kernel/prebuilts/"
+if [ ! -d build-tools ]; then
+    git clone https://android.googlesource.com/kernel/prebuilts/build-tools
+else
+    echo "[✔] build-tools already exists."
+fi
+
+echo "[I] Creating GKI directory..."
+mkdir -vp "$my_top_dir/vendor/aosp_gki/kernel/aarch64"
+cd "$my_top_dir/vendor/aosp_gki/kernel/aarch64"
+
+echo "[I] Downloading GKI Image.gz for android13-5.10-2025-07_r1..."
+wget -O Image.gz \
+  https://ci.android.com/builds/submitted/13792087/kernel_aarch64/latest/Image.gz
+
+echo "[✔] All prebuilts and GKI downloaded successfully."
+cd "$my_top_dir"
+
--- helpers/download_toolchain.sh
+++ helpers/download_toolchain.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+my_top_dir=$PWD
 
 echo "[*] Cloning toolchains..."
 
-mkdir -vp "$my_top_dir/prebuilts/clang/host/clang-r416183b"
+mkdir -vp "$my_top_dir/prebuilts/clang/host/linux-x86/clang-r416183b"
+
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/b669748458572622ed716407611633c5415da25c/clang-r416183b.tar.gz \
      -O "$my_top_dir/prebuilts/clang/host/linux-x86/clang-r416183b/clang-r416183b.tar.gz"
 tar -xf "$my_top_dir/prebuilts/clang/host/linux-x86/clang-r416183b/clang-r416183b.tar.gz" \
